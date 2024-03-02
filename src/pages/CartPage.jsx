5cd4ff8a4be3153c04efdc89e23abe9e3016d176
Priyojit <priyojitsingha01@gmail.com>
2024-03-02 15:04:32 +0530

    style: size of item-tile changed in cart page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -17,7 +17,7 @@ export default function CartPage() {
             <h1 className="text-2xl font-bold">Cart Page</h1>
             <div className="">
                 {items.map(item => (
-                    <div className='bg-slate-100 my-1 shadow-lg rounded-md py-3 px-4 flex justify-between' key={item.id}>
+                    <div className='bg-slate-100 my-1 max-w-80 mx-auto shadow-lg rounded-md py-3 px-4 flex justify-between' key={item.id}>
                         <div className="flex">
                             <button onClick={() => removeItems(item.name)} className='text-gray-500'>×</button>
                             <h1 className="font-semibold px-2 text-gray-700">{item.name}</h1>
@@ -25,7 +25,7 @@ export default function CartPage() {
                         <p className="">{item.price}$</p>
                     </div>
                 ))}
-                {totalPrice != 0 ? <p className='text-right py-4 font-bold text-gray-700'>Total: {totalPrice}$</p> :
+                {totalPrice != 0 ? <p className='text-right md:text-center py-4 font-bold text-gray-700'>Total: {totalPrice}$</p> :
                     <p className='text-center py-3'>No Items in Cart</p>}
             </div>
         </div>
